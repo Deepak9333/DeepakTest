8ac7e2aead6f42b2f7a5facb6fd84f09f95da88e
apidemo <deepakkumarnaik3@gmail.com>
2025-02-14 01:01:21 +0530

    Aws deployemnt test New ec2

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("Hello Java welcome to My AWS Deployment .");
+        System.out.println("Hello Java welcome to My AWS Deployment EC2.");
     }
 
 }
